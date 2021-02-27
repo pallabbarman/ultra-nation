775fdbdfed0beafd7c59dee07f430f047b5ca7c9
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-02-27 10:12:49 +0600

    country show

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Country from './components/Country/Country';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        fetch('https://restcountries.eu/rest/v2/all')
+            .then((res) => res.json())
+            .then((data) => setCountries(data))
+            .catch((err) => console.log(err));
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Countries: {countries.length}</h1>
+            {countries.map((country) => (
+                <Country name={country.name} />
+            ))}
+        </div>
+    );
 }
 
 export default App;
